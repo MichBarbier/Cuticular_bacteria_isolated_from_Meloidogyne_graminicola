b06abc8be2e527ecc5a8e80acc22840c2eba00b0
Michel Barbier <BarbierMichl@gmail.com>
2025-10-06 11:34:14 +0200

    Update 01_Soil_antagonism.R

--- Scripts/01_Soil_antagonism.R
+++ Scripts/01_Soil_antagonism.R
@@ -18,19 +18,17 @@ Fills <- c("orangered", "white", "lightskyblue", "white")
 
 ### Boxplot
 p1 <- ggplot(Data)+
-  geom_boxplot(aes(x = Samples, y = Percentage_of_mobile_larvae, color = Samples, fill = Samples), alpha = 0.65)+
+  geom_boxplot(aes(x = Samples, y = Percentage_of_mobile_larvae, fill = Samples), alpha = 0.65, color = "black")+
   scale_fill_manual("Samples", values = Fills, guide = "none")+
-  scale_color_manual("Samples", values = Colors, guide = "none")+
   geom_signif(aes(x = Samples, y = Percentage_of_mobile_larvae), test = wilcox.test, comparisons = list(c("CT", "CA"), c("CT Filtered", "CT"), c("CA Filtered", "CA")), map_signif_level = TRUE, y_position = c(165, 145, 145), textsize = 5)+
-  geom_point(aes(x = Samples, y = Percentage_of_mobile_larvae, color = Samples))+
-  scale_color_manual("Samples", values = Colors, guide = "none")+
-  stat_summary(aes(x = Samples, y = Percentage_of_mobile_larvae), fun = mean, colour = "black",  geom = "point", shape = 3, size = 2)+
-  ylab("Percentage of mobile larvae")+
+  geom_point(aes(x = Samples, y = Percentage_of_mobile_larvae), color = "black")+
+  stat_summary(aes(x = Samples, y = Percentage_of_mobile_larvae), fun = mean, colour = "red",  geom = "point", shape = 3, size = 2)+
+  ylab("Mobile J2")+
+  scale_y_continuous(breaks = seq(0, 200, 50))+
   ylim(0,175)+
   xlab("")+
   theme_bw()+
   theme(axis.text.x = element_text(size = 12, color = Colors), axis.title.x = element_blank(), axis.ticks.x = element_blank(), axis.text.y = element_text(size = 12), axis.title.y = element_text(size = 13), legend.title = element_blank())
 p1
 
-ggsave(plot = p1, dpi = 1000, device = "jpeg", width = 7, height = 4, filename = "Results/Figure_1.jpeg")
-
+ggsave(plot = p1, dpi = 1000, device = "jpeg", width = 7, height = 6, filename = "Results/Figure 1.jpeg")
